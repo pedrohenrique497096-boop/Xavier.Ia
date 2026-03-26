be7e0ed6e97cd3390239b01f5357c7747db614d1
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-26 06:07:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+from fastapi import FastAPI
+from brain import generate_signal
+from market import get_data
 
+app = FastAPI()
+
+@app.get("/signal/{symbol}")
+def get_signal(symbol: str):
+    df = get_data(symbol)
+
+    signal = generate_signal(df)
+
+    if signal:
+        return {"status": "signal", "data": signal}
+
+    return {"status": "no_trade"}
