79a78e650648081ad0befc01e79eb71821062106
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-26 06:09:46 -0300

    Update market.py

--- market.py
+++ market.py
@@ -1,8 +1,8 @@
 import requests
 import pandas as pd
 
-def get_data(symbol):
-    url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval=5m&limit=100"
+def get_data(symbol, interval="5m", limit=100):
+    url = f"https://api.binance.com/api/v3/klines?symbol={symbol}&interval={interval}&limit={limit}"
     data = requests.get(url).json()
 
     df = pd.DataFrame(data, columns=[
@@ -10,8 +10,15 @@ def get_data(symbol):
         "ct","qv","n","tb","tq","ig"
     ])
 
-    df["close"] = df["close"].astype(float)
+    df["open"] = df["open"].astype(float)
     df["high"] = df["high"].astype(float)
     df["low"] = df["low"].astype(float)
+    df["close"] = df["close"].astype(float)
 
     return df
+
+def get_multi_tf(symbol):
+    df_m5 = get_data(symbol, "5m")
+    df_h1 = get_data(symbol, "1h")
+
+    return df_m5, df_h1
