a8d5f4fb74ba22f0e1bec53d361acaa8b1bebc5f
pedrohenrique497096-boop <pedrohenrique497096@gmail.com>
2026-03-26 06:10:11 -0300

    Update get_signal to use multi-timeframe data

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from brain import generate_signal
-from market import get_data
+from market import get_multi_tf
 
 app = FastAPI()
 
 @app.get("/signal/{symbol}")
 def get_signal(symbol: str):
-    df = get_data(symbol)
 
-    signal = generate_signal(df)
+    df_m5, df_h1 = get_multi_tf(symbol)
+
+    signal = generate_signal(df_m5, df_h1)
 
     if signal:
         return {"status": "signal", "data": signal}
